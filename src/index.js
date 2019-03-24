17331ede064650d4857c2ce9ebfa4ce1f288262c
Aliaksei Housha <gunichone@gmail.com>
2019-03-24 21:28:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let count = 0;                                 // задаем переменную count, чтобы считать спичи, которые участвуют в треугольниках
+  for (let i = 0; i < preferences.length; i++) { // перебираем все элементы в массиве
+      let Spich = preferences[i] - 1;            // создаем переменную спич и присваиваем значение индекса элемента минус один
+        if (Spich == i)                          // делаем проверку спич равен ли индексу
+          continue;                              // если значение спичона равно позиции двигаемся дальше
+      let Spich2 = preferences[Spich] - 1;       // создаем перем. спич2 и добавляем значение индекса первого спича минус 1
+        if (Spich2 == Spich) 
+            continue;
+      let Spich3 = preferences[Spich2] - 1;
+        if (Spich3 == Spich2) 
+          continue;
+      if (Spich3 == i) {                          // если проверка прошла, добавляем спич в каунт
+          count++;
+      }
+   }
+   return count / 3;                              // узнаем количество треугольников из трех спичонов
+}
